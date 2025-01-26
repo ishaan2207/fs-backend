d5dd901edb20e7ebf3c57446bd654496b08531cb
Ishaan Gupta <ishaangupta02@gmail.com>
2025-01-26 18:20:49 +0530

    Created jobs model

---
+++ src/models/Job.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    company: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+})
+
+const Job = mongoose.model('Job', jobSchema);
+
+module.exports = Job;
